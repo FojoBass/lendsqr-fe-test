066ce7d7a400d977a0b668b5d90132ef770736a7
Fojobass <Fojo4god@gmail.com>
2024-04-26 17:43:35 +0100

    Working on Dashboard

--- src/pages/dashboard/DashNav.tsx
+++ src/pages/dashboard/DashNav.tsx
@@ -1,7 +1,61 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import Logo from '../../assets/icons/logo.svg?react';
+import { FaCaretDown, FaSearch } from 'react-icons/fa';
+import { Bell } from '../../icons';
+import avi from '../../assets/images/image 4.png';
+import { MdMenu } from 'react-icons/md';
 
 const DashNav = () => {
-  return <div>DashNav</div>;
+  return (
+    <nav className='dash_nav'>
+      <div className='top'>
+        <div className='left_side'>
+          <Link to='/dashboard' className='logo'>
+            <Logo />
+          </Link>
+
+          <div className='search_wrapper'>
+            <input type='text' placeholder='Search for anything' />
+            <button className='search_btn'>
+              <FaSearch />
+            </button>
+          </div>
+        </div>
+
+        <div className='right_side'>
+          <Link className='docs' to='/dashboard'>
+            Docs
+          </Link>
+
+          <button className='notification_btn'>
+            <Bell />
+          </button>
+
+          <div className='user_info'>
+            <div className='img_wrapper'>
+              <img src={avi} alt='user avi' />
+            </div>
+            <p>
+              Adedeji
+              <span className='icon'>
+                <FaCaretDown />
+              </span>
+            </p>
+          </div>
+
+          <button className='menu_btn'>
+            <MdMenu />
+          </button>
+        </div>
+      </div>
+      <div className='bottom'>
+        <input type='text' placeholder='Search for anything' />
+        <button className='search_btn'>
+          <FaSearch />
+        </button>
+      </div>
+    </nav>
+  );
 };
 
 export default DashNav;
